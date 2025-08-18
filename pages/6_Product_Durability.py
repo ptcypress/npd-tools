0463eae3706e2f9af57b89e2404469b92818446c
ptcypress <cmitra86@gmail.com>
2025-08-18 09:33:22 -0400

    Update 6_Product_Durability.py

--- pages/6_Product_Durability.py
+++ pages/6_Product_Durability.py
@@ -8,7 +8,7 @@ import os
 
 # Streamlit page config
 st.set_page_config(page_title="Material Loss Over Time", layout="wide")
-st.title("Material Loss Over Time")
+st.title("Product Durability - AngleOn™")
 
 # Radio button to choose unit
 unit_choice = st.radio("Select Y-axis unit:", ["Mat'l Loss (%)", "Mat'l Loss (in)"])
